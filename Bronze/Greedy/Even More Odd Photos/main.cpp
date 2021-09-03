e7062b40241b23e258455806c8b5c8cf0f754622
Abdelrahman-Yehia <a.k.yehia3010@gmail.com>
2021-09-03 08:25:33 +0200

    Commit

---
+++ Bronze/Greedy/Even More Odd Photos/main.cpp
@@ -0,0 +1,255 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define pb push_back
+#define F first
+#define S second
+#define rep(i,n) for(int i = 0; i < (n); i++)
+
+#define input            freopen("input.in","r",stdin)
+#define output           freopen("output.out","w",stdout)
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+using ll = long long;
+using ld = long double;
+using ul = unsigned long long;
+using ii = pair<int, int>;
+
+const ll mod7 = 1000000007,mod1 = 1000000000,OO = 0x3f3f3f3f;
+const ld EPS = 1e-7;
+double Pi=acos(-1);
+const ld rad=Pi/180.0;
+
+long long power(long long a, long long  b, ll mod)
+{
+    if(!b)
+        return 1ll;
+    if(b == 1)
+        return a%mod;
+    long long r = power(a, b/2ll,mod)%mod;
+    if(b%2ll)
+        return ((r*(a%mod)%mod)*r)%mod;
+    else
+        return (r*r)%mod;
+
+}
+
+ll nCr (ll n,ll m)
+{
+    return(m == 0)?1:n*nCr(n-1,m-1)/m;
+}
+
+bool checkDivisibility(ll n, int digit)
+{
+    return (digit == 0|| (digit != 0 && n % digit == 0));
+}
+
+bool allDigitsDivide(ll n)
+{
+    ll temp = n;
+    while (temp > 0)
+    {
+
+        ll digit = temp % 10;
+        if (!(checkDivisibility(n, digit)))
+            return false;
+
+        temp /= 10;
+    }
+    return true;
+}
+
+bool comp(pair<ld,ld> a,pair<ld,ld> b)
+{
+    return abs(a.F-a.S) > abs(b.F-b.S);
+}
+
+bool comp2(ll a,ll b)
+{
+    return a > b;
+}
+
+int gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+int power2(int a, unsigned int n, int p)
+{
+    int res = 1;      // Initialize result
+    a = a % p;  // Update 'a' if 'a' >= p
+
+    while (n > 0)
+    {
+        // If n is odd, multiply 'a' with result
+        if (n & 1)
+            res = (res*a) % p;
+
+        // n must be even now
+        n = n>>1; // n = n/2
+        a = (a*a) % p;
+    }
+    return res;
+}
+
+
+
+// If n is prime, then always returns true, If n is
+// composite than returns false with high probability
+// Higher value of k increases probability of correct
+// result.
+bool isPrime(unsigned int n, int k)
+{
+    // Corner cases
+    if (n <= 1 || n == 4)
+        return false;
+    if (n <= 3)
+        return true;
+
+    // Try k times
+    while (k>0)
+    {
+        // Pick a random number in [2..n-2]
+        // Above corner cases make sure that n > 4
+        int a = 2 + rand()%(n-4);
+
+        // Checking if a and n are co-prime
+        if (gcd(n, a) != 1)
+            return false;
+
+        // Fermat's little theorem
+        if (power2(a, n-1, n) != 1)
+            return false;
+
+        k--;
+    }
+
+    return true;
+}
+
+vector<int> primes;
+
+void SieveOfEratosthenes(int n)
+{
+    // Create a boolean array
+    // "prime[0..n]" and initialize
+    // all entries it as true.
+    // A value in prime[i] will
+    // finally be false if i is
+    // Not a prime, else true.
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        // If prime[p] is not changed,
+        // then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples
+            // of p greater than or
+            // equal to the square of it
+            // numbers which are multiple
+            // of p and are less than p^2
+            // are already been marked.
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // Print all prime numbers
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            primes.pb(p);
+}
+
+
+
+ld solveQuad(ld a,ld b,ld c)
+{
+    ld x1, x2, discriminant, realPart, imaginaryPart;
+    discriminant = b*b - 4*a*c;
+
+    if (discriminant > 0)
+    {
+        x1 = (-b + sqrt(discriminant)) / (2*a);
+        x2 = (-b - sqrt(discriminant)) / (2*a);
+        return(max(x1,x2));
+    }
+
+    else if (discriminant == 0)
+    {
+        x1 = -b/(2*a);
+        return x1;
+    }
+
+}
+
+/**************************************************/
+
+const ll N = 1e3+5;
+
+int a[N];
+
+void solve(int x,int t)
+{
+    int n;
+    cin>>n;
+    int odds = 0, evens = 0;
+    rep(i,n){
+        cin>>a[i];
+        if(a[i]%2)
+            odds++;
+        else evens++;
+    }
+    int ans = 0;
+    bool odd = 0;
+    while(true){
+        if(!odd){
+            odd = !odd;
+            if(evens>0){
+                evens--;
+                ans++;
+            }
+            else if(odds>1){
+                odds-=2;
+                ans++;
+            }
+            else break;
+        }
+        else{
+            odd = !odd;
+            if(odds>0){
+                odds--;
+                ans++;
+            }
+            else break;
+        }
+    }
+
+    ans-=odds;
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    //freopen("cowtip.in","r",stdin);
+    //freopen("cowtip.out","w",stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+    int t = 0;
+    //cin>>t;
+    int x = t;
+    //while(t--)
+    solve(x,t);
+}
